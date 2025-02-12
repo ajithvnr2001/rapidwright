47459e1a1656a716f4735e3a51a650e0830df637
ajithvnr2001 <74783525+ajithvnr2001@users.noreply.github.com>
2025-02-12 14:49:11 +0530

    Update pdf_generator.py

--- autopdf/agents/pdf_generator.py
+++ autopdf/agents/pdf_generator.py
@@ -1,7 +1,7 @@
 from crewai import Agent
 from core.pdf_utils import create_pdf_from_text, create_pdf_from_html
 from langchain.tools import tool
-from typing import Dict, ClassVar
+from typing import ClassVar  # Import ClassVar
 
 class PDFGeneratorAgent(Agent):
     def __init__(self):
@@ -14,15 +14,30 @@ class PDFGeneratorAgent(Agent):
             verbose=True,
             allow_delegation=False
         )
-    
     @tool
     def create_pdf_from_text_tool_method(self, content: str, title: str = "Incident Report") -> bytes:
+        """Creates a PDF from text content.
+        Args:
+            content (str): The text content of the PDF.
+            title (str): the title of the document (optional)
+        Returns:
+            bytes: The PDF file as bytes.
+        """
+
         if not content:
             raise ValueError("Content is required for PDF Generation.")
+
         return create_pdf_from_text(content, title)
-    
     @tool
     def create_pdf_from_html_tool_method(self, content: str, title: str = "Incident Report") -> bytes:
+        """Creates a PDF from HTML content.
+        Args:
+            content (str): The HTML content of the PDF.
+            title (str): the title of the document (optional)
+        Returns:
+            bytes: The PDF file as bytes.
+        """
         if not content:
             raise ValueError("Content is required for PDF Generation.")
+
         return create_pdf_from_html(content, title)
