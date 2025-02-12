aa64db9b63994adc3ba898eb4f8642c5b402a372
ajithvnr2001 <74783525+ajithvnr2001@users.noreply.github.com>
2025-02-12 11:35:52 +0530

    Update config.py

--- autopdf/core/config.py
+++ autopdf/core/config.py
@@ -5,18 +5,18 @@ from pydantic import BaseSettings, Field
 load_dotenv()
 
 class Settings(BaseSettings):
-    glpi_url: str = Field(..., env="GLPI_URL")
-    glpi_app_token: str = Field(..., env="GLPI_APP_TOKEN")
-    glpi_user_token: str = Field(..., env="GLPI_USER_TOKEN")
-    meilisearch_url: str = Field(..., env="MEILISEARCH_URL")
-    meilisearch_master_key: str = Field(..., env="MEILISEARCH_MASTER_KEY")
-    wasabi_endpoint: str = Field(..., env="WASABI_ENDPOINT")
-    wasabi_access_key: str = Field(..., env="WASABI_ACCESS_KEY")
-    wasabi_secret_key: str = Field(..., env="WASABI_SECRET_KEY")
-    openai_api_base: str = Field(..., env="OPENAI_API_BASE")
-    openai_api_key: str = Field(..., env="OPENAI_API_KEY")
-    model_name: str = Field(..., env="MODEL_NAME")
-    bucket_name: str = Field(..., env="BUCKET_NAME")
+    glpi_url: str = "https://ltimindtree.in1.glpi-network.cloud"
+    glpi_app_token: str = "bnVEfEQ6n7kYpzFCEiF1UgCCWJkd5bWyU7UiQXd3"
+    glpi_user_token: str = "hN04d5lgFrkmprGz183sPfUhXt04102DF10pUFsN"
+    meilisearch_url: str = "https://ms-6c0bfee744cb-18789.nyc.meilisearch.io"
+    meilisearch_master_key: str = "289e33eaa93280a3955fbed03a73fce65048d51c"
+    wasabi_endpoint: str = "s3.ap-northeast-1.wasabisys.com"
+    wasabi_access_key: str = "WFDR91CL1S6EII3LFKHV"
+    wasabi_secret_key: str = "rGnTDbiXOm3IFpRiaAZm9jGkfXJTGpylrJbuZ0et"
+    openai_api_base: str = "https://chatapi.akash.network/api/v1"
+    openai_api_key: str = "sk-lw2UYlqYhjaXkboZ0pHv9A"
+    model_name: str = "DeepSeek-R1"
+    bucket_name: str = "rapidwrite"
     max_rag_iterations: int = 3
 
 settings = Settings()
